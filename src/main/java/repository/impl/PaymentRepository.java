8a8b56a357cb76997bb611b4743c42dfcc032fbf
Jawaadallie <230472125@mycput.ac.za>
2026-03-25 18:17:46 +0200

    Payment Repository

---
+++ src/main/java/repository/impl/PaymentRepository.java
@@ -0,0 +1,11 @@
+/* PaymentRepository.java
+   This is the repository interface for Payment.
+   Author: Mogamad Jawaad Allie - 230671942
+   Date: 25 March 2026
+*/
+package repository;
+
+import domain.Payment;
+
+public interface PaymentRepository extends IRepository<Payment, String> {
+}
